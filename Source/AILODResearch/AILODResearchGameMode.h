0b0c0c5d8d030eb324bb31054ad058376b917096
Jadey0804 <2651466805@qq.com>
2026-07-24 19:17:05 +0100

    Initial commit

---
+++ Source/AILODResearch/AILODResearchGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AILODResearchGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAILODResearchGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAILODResearchGameMode();
+};
+
+
+
